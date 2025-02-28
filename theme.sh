73cd1bc98900d05b2e7ce60a60d57f4b057e7f80
0xFFFFFF <52132640+0x-FFFFFF@users.noreply.github.com>
2025-02-27 20:28:55 -0700

    Add uninstall option

--- theme.sh
+++ theme.sh
@@ -1,5 +1,19 @@
 #!/bin/env bash
 
+# Icons destination directory
+ICON_DEST_DIR="/home/$SUDO_USER/.icons"
+# [ "$EUID" -eq 0 ] && ICON_DEST_DIR="/usr/share/icons"
+
+THEME_DEST_DIR="/home/$SUDO_USER/.themes"
+# [ "$EUID" -eq 0 ] && THEME_DEST_DIR="/usr/share/themes"
+
+if [[ " $@ " =~ " -u " ]]; then
+    echo "Uninstalling themes."
+    rm -rf "$ICON_DEST_DIR/Catppuccin*"
+    rm -rf "$THEME_DEST_DIR/Catppuccin*"
+    exit 0
+fi
+
 DEPS="git gnome-tweaks gnome-shell-extensions"
 THEME_DEPS="sassc gnome-themes-extra gtk2-engines-murrine"
 INSTALLED_DEPS=$(dpkg-query -W -f='${binary:Package}\n')
@@ -22,13 +36,6 @@ if [ ${#TO_BE_INSTALLED_DEPS[@]} -gt 0 ]; then
     sudo apt install "${TO_BE_INSTALLED_DEPS[@]}" -y
 fi
 
-# Icons destination directory
-ICON_DEST_DIR="/home/$SUDO_USER/.icons"
-# [ "$EUID" -eq 0 ] && ICON_DEST_DIR="/usr/share/icons"
-
-THEME_DEST_DIR="/home/$SUDO_USER/.themes"
-# [ "$EUID" -eq 0 ] && THEME_DEST_DIR="/usr/share/themes"
-
 if [ -d "Catppuccin-GTK-Theme" ]; then
     echo "Directory 'Catppuccin-GTK-Theme' already exists, skipping git clone."
 else
@@ -44,7 +51,7 @@ if [ ! -d "$ICON_DEST_DIR" ]; then
 fi
 
 echo "Copying icons to $ICON_DEST_DIR"
-cp -ra ./Catppuccin-GTK-Theme/icons/* "$ICON_DEST_DIR"
+cp -ra ./Catppuccin-GTK-Theme/icons/* $ICON_DEST_DIR
 
-echo "Cleaning up"
+echo "Cleaning up."
 rm -rf ./Catppuccin-GTK-Theme
